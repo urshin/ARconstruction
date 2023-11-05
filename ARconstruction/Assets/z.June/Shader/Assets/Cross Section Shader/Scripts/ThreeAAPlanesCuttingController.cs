83f915f0a973ee46f9c3d0b03447d1d6edd45bf7
urshin <yesjune77@gmail.com>
2023-11-05 18:43:03 +0900

    XYZ 값 슬라이드 연결
    x,y,z,값으로 오브젝트 사라지게 만듬
    각 슬라이드로 조절 가능

---
+++ ARconstruction/Assets/z.June/Shader/Assets/Cross Section Shader/Scripts/ThreeAAPlanesCuttingController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThreeAAPlanesCuttingController : MonoBehaviour
+{
+
+    public GameObject planeYZ;
+    public GameObject planeXZ;
+    public GameObject planeXY;
+    public Material mat;
+    public Vector3 positionYZ;
+    public Vector3 positionXZ;
+    public Vector3 positionXY;
+    public Renderer rend;
+    public GameObject RenderingObject;
+    // Use this for initialization
+    void Start()
+    {
+        rend = RenderingObject.GetComponent<Renderer>();
+        Debug.Log(rend.material.name);
+        mat = rend.material;
+        Debug.Log(mat.name);
+        UpdateShaderProperties();
+    }
+    void Update()
+    {
+        UpdateShaderProperties();
+    }
+
+    private void UpdateShaderProperties()
+    {
+        positionYZ = planeYZ.transform.position;
+        positionXZ = planeXZ.transform.position;
+        positionXY = planeXY.transform.position;
+
+        if (mat.shader.name == "CrossSection/ThreeAAPlanesBSP")
+        {
+           mat.SetVector("_Plane1Position", positionYZ);
+           mat.SetVector("_Plane2Position", positionXZ);
+            mat.SetVector("_Plane3Position", positionXY);
+        }
+
+        //for (int i = 0; i < rend.materials.Length; i++)
+        //{
+        //    if (rend.materials[i].shader.name == "CrossSection/ThreeAAPlanesBSP")
+        //    {
+        //        rend.materials[i].SetVector("Plane1Position", positionYZ);
+        //        rend.materials[i].SetVector("Plane2Position", positionXZ);
+        //        rend.materials[i].SetVector("Plane3Position", positionXY);
+        //    }
+        //}
+
+    }
+}
