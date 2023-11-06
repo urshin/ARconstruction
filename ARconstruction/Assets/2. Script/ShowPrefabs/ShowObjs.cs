957a44d1f2834071a3330688c28cd2d8e31556c8
poiz1245 <wva960@gmail.com>
2023-11-06 16:26:27 +0900

    SJH

--- ARconstruction/Assets/2. Script/ShowPrefabs/ShowObjs.cs
+++ ARconstruction/Assets/2. Script/ShowPrefabs/ShowObjs.cs
@@ -4,29 +4,29 @@ using UnityEngine.UI;
 public class ShowObjs : MonoBehaviour
 {
     #region Toggle
-    [SerializeField] Toggle m_Toggle_ViewAll;
-    [SerializeField] Toggle m_Toggle_Frame;
-    [SerializeField] Toggle m_Toggle_Wall;
-    [SerializeField] Toggle m_Toggle_MEPF;
-    [SerializeField] Toggle m_Toggle_Mechanical;
-    [SerializeField] Toggle m_Toggle_Plumbing;
-    [SerializeField] Toggle m_Toggle_FireProtection;
+    [SerializeField] Toggle m_Toggle_ViewAll; // 모든 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_Frame; // 프레임 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_Wall; // 벽 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_MEPF; // MEPF 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_Mechanical; // 기계 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_Plumbing; // 배관 오브젝트를 보여주는 토글
+    [SerializeField] Toggle m_Toggle_FireProtection; // 소방 오브젝트를 보여주는 토글
     #endregion
 
     #region GameObject
-    [SerializeField] GameObject m_withoutWall;
-    [SerializeField] GameObject m_FrameObj;
-    [SerializeField] GameObject m_WallObj;
-    [SerializeField] GameObject m_MechanicalObj;
-    [SerializeField] GameObject m_PlumbingObj;
-    [SerializeField] GameObject m_FireProtectionObj;
+    [SerializeField] GameObject m_withoutWall; // 벽 없이 보여지는 오브젝트
+    [SerializeField] GameObject m_FrameObj; // 프레임 오브젝트
+    [SerializeField] GameObject m_WallObj; // 벽 오브젝트
+    [SerializeField] GameObject m_MechanicalObj; // 기계 오브젝트
+    [SerializeField] GameObject m_PlumbingObj; // 배관 오브젝트
+    [SerializeField] GameObject m_FireProtectionObj; // 소방 오브젝트
     #endregion
 
     void Start()
     {
-        ToggleInit();
+        ToggleInit(); // 토글 초기 설정을 호출
 
-        //각 토글에 대한 이벤트 리스너를 추가
+        // 각 토글에 대한 이벤트 리스너를 추가
         Toggle[] toggles = new Toggle[]
         { m_Toggle_ViewAll, m_Toggle_Frame, m_Toggle_Wall, m_Toggle_MEPF, m_Toggle_Mechanical, m_Toggle_Plumbing, m_Toggle_FireProtection };
 
@@ -34,15 +34,15 @@ public class ShowObjs : MonoBehaviour
         {
             toggle.onValueChanged.AddListener(delegate
             {
-                ToggleValueChanged(toggle);
+                ToggleValueChanged(toggle); // 토글 값 변경 이벤트 핸들러를 등록
             });
         }
     }
 
     void ToggleInit()
     {
-        //토글 초기 설정
-        m_Toggle_ViewAll.isOn = true;
+        // 토글 초기 설정
+        m_Toggle_ViewAll.isOn = true; // 처음에 모두 보이게 설정
         m_Toggle_Frame.isOn = false;
         m_Toggle_Wall.isOn = false;
         m_Toggle_Mechanical.isOn = false;
@@ -56,11 +56,12 @@ public class ShowObjs : MonoBehaviour
         {
             bool viewAllEnabled = toggle.isOn;
 
-            m_withoutWall.SetActive(viewAllEnabled);
-            m_WallObj.SetActive(viewAllEnabled);
+            m_withoutWall.SetActive(viewAllEnabled); // 벽 없이 보여지는 오브젝트 활성화/비활성화
+            m_WallObj.SetActive(viewAllEnabled); // 벽 오브젝트 활성화/비활성화
 
             if (viewAllEnabled)
             {
+                // 모든 다른 토글들을 비활성화
                 m_Toggle_Frame.isOn = false;
                 m_Toggle_Wall.isOn = false;
                 m_Toggle_MEPF.isOn = false;
@@ -73,20 +74,23 @@ public class ShowObjs : MonoBehaviour
         {
             if (toggle.isOn)
             {
+                // 다른 토글 비활성화
                 m_Toggle_ViewAll.isOn = false;
             }
-            m_FrameObj.SetActive(toggle.isOn);
+            m_FrameObj.SetActive(toggle.isOn); // 프레임 오브젝트 활성화/비활성화
         }
         else if (toggle == m_Toggle_Wall)
         {
             if (toggle.isOn)
             {
+                // 다른 토글 비활성화
                 m_Toggle_ViewAll.isOn = false;
             }
-            m_WallObj.SetActive(toggle.isOn);
+            m_WallObj.SetActive(toggle.isOn); // 벽 오브젝트 활성화/비활성화
         }
         else if (toggle == m_Toggle_MEPF && !toggle.isOn)
         {
+            // MEPF 토글이 해제되면 다른 하위 토글들도 해제
             m_Toggle_Mechanical.isOn = false;
             m_Toggle_Plumbing.isOn = false;
             m_Toggle_FireProtection.isOn = false;
@@ -95,25 +99,28 @@ public class ShowObjs : MonoBehaviour
         {
             if (toggle.isOn)
             {
+                // 다른 토글 비활성화
                 m_Toggle_ViewAll.isOn = false;
             }
-            m_MechanicalObj.SetActive(toggle.isOn);
+            m_MechanicalObj.SetActive(toggle.isOn); // 기계 오브젝트 활성화/비활성화
         }
         else if (toggle == m_Toggle_Plumbing)
         {
             if (toggle.isOn)
             {
+                // 다른 토글 비활성화
                 m_Toggle_ViewAll.isOn = false;
             }
-            m_PlumbingObj.SetActive(toggle.isOn);
+            m_PlumbingObj.SetActive(toggle.isOn); // 배관 오브젝트 활성화/비활성화
         }
         else if (toggle == m_Toggle_FireProtection)
         {
             if (toggle.isOn)
             {
+                // 다른 토글 비활성화
                 m_Toggle_ViewAll.isOn = false;
             }
-            m_FireProtectionObj.SetActive(toggle.isOn);
+            m_FireProtectionObj.SetActive(toggle.isOn); // 소방 오브젝트 활성화/비활성화
         }
     }
 }
