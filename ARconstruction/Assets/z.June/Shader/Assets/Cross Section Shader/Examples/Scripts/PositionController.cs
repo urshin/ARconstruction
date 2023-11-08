fcc3dbab940d4c0c6e63aca6684302b5a073d69f
urshin <136461531+urshin@users.noreply.github.com>
2023-11-08 10:53:28 +0900

    xyz완성

--- ARconstruction/Assets/z.June/Shader/Assets/Cross Section Shader/Examples/Scripts/PositionController.cs
+++ ARconstruction/Assets/z.June/Shader/Assets/Cross Section Shader/Examples/Scripts/PositionController.cs
@@ -6,10 +6,13 @@ public class PositionController : MonoBehaviour {
     public Slider XPos;
     public Slider YPos;
     public Slider ZPos;
+    public float x;
+    public float y;
+    public float z;
     public Transform ControlledObject;
     public void UpdateObjectPosition(Transform ControlledObject)
     {
-        Vector3 newPosition = new Vector3(XPos?XPos.value:0, YPos?YPos.value:0, ZPos?ZPos.value:0);
+        Vector3 newPosition = new Vector3(XPos?XPos.value:x, YPos?YPos.value:y, ZPos?ZPos.value:z);
         ControlledObject.localPosition = newPosition;
     }
   
--- ARconstruction/Assets/z.June/Shader/Assets/Cross Section Shader/Scripts/GenericThreePlanesCuttingController.cs
+++ ARconstruction/Assets/z.June/Shader/Assets/Cross Section Shader/Scripts/GenericThreePlanesCuttingController.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GenericThreePlanesCuttingController : MonoBehaviour
 {
     public GameObject plane1; // 첫 번째 평면
     public GameObject plane2; // 두 번째 평면
     public GameObject plane3; // 세 번째 평면
-    public Renderer rend; // 렌더러 컴포넌트
-    public Vector3 normal1; // 첫 번째 평면의 법선
-    public Vector3 position1; // 첫 번째 평면의 위치
-    public Vector3 normal2; // 두 번째 평면의 법선
-    public Vector3 position2; // 두 번째 평면의 위치
-    public Vector3 normal3; // 세 번째 평면의 법선
-    public Vector3 position3; // 세 번째 평면의 위치
+    public GameObject[] RenderingObject;
+    [SerializeField] Renderer[] rend; // 렌더러 컴포넌트 배열
+    [SerializeField] Material[] material;
 
     // Use this for initialization
     void Start()
     {
-        rend = GetComponent<Renderer>(); // 스크립트가 연결된 게임 오브젝트의 렌더러 컴포넌트 가져오기
+        rend = new Renderer[RenderingObject.Length]; // 배열 크기 초기화
+
+        for (int i = 0; i < RenderingObject.Length; i++)
+        {
+            rend[i] = RenderingObject[i].GetComponent<Renderer>();
+        }
+
+        // Create a list to hold the materials
+        List<Material> materialList = new List<Material>();
+
+        // Copy materials from rend to material list
+        for (int i = 0; i < rend.Length; i++)
+        {
+            Material[] materials = rend[i].materials; // Get all materials for the renderer
+
+            // Add each material to the list
+            materialList.AddRange(materials);
+        }
+
+        // Convert the list to an array
+        material = materialList.ToArray();
+
         UpdateShaderProperties(); // 쉐이더 속성 업데이트 함수 호출
     }
 
@@ -29,29 +47,29 @@ public class GenericThreePlanesCuttingController : MonoBehaviour
     private void UpdateShaderProperties()
     {
         // 평면 1의 법선과 위치를 업데이트
-        normal1 = plane1.transform.TransformVector(new Vector3(0, 0, -1));
-        position1 = plane1.transform.position;
+        Vector3 normal1 = plane1.transform.TransformVector(new Vector3(0, 0, -1));
+        Vector3 position1 = plane1.transform.position;
 
         // 평면 2의 법선과 위치를 업데이트
-        normal2 = plane2.transform.TransformVector(new Vector3(0, 0, -1));
-        position2 = plane2.transform.position;
+        Vector3 normal2 = plane2.transform.TransformVector(new Vector3(0, 0, -1));
+        Vector3 position2 = plane2.transform.position;
 
         // 평면 3의 법선과 위치를 업데이트
-        normal3 = plane3.transform.TransformVector(new Vector3(0, 0, -1));
-        position3 = plane3.transform.position;
+        Vector3 normal3 = plane3.transform.TransformVector(new Vector3(0, 0, -1));
+        Vector3 position3 = plane3.transform.position;
 
         // 모든 Material 배열을 순회하며 CrossSection/GenericThreePlanesBSP 쉐이더를 찾아 속성 업데이트
-        for (int i = 0; i < rend.materials.Length; i++)
+        for (int i = 0; i < material.Length; i++)
         {
-            if (rend.materials[i].shader.name == "CrossSection/GenericThreePlanesBSP")
+            if (material[i].shader.name == "CrossSection/GenericThreePlanesBSP")
             {
                 // 쉐이더 속성 업데이트
-                rend.materials[i].SetVector("_Plane1Normal", normal1);
-                rend.materials[i].SetVector("_Plane1Position", position1);
-                rend.materials[i].SetVector("_Plane2Normal", normal2);
-                rend.materials[i].SetVector("_Plane2Position", position2);
-                rend.materials[i].SetVector("_Plane3Normal", normal3);
-                rend.materials[i].SetVector("_Plane3Position", position3);
+                material[i].SetVector("_Plane1Normal", normal1);
+                material[i].SetVector("_Plane1Position", position1);
+                material[i].SetVector("_Plane2Normal", normal2);
+                material[i].SetVector("_Plane2Position", position2);
+                material[i].SetVector("_Plane3Normal", normal3);
+                material[i].SetVector("_Plane3Position", position3);
             }
         }
     }
