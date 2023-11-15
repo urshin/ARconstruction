31353eadcae6ec791854bacc0cdf5e7463421dd8
poiz1245 <wva960@gmail.com>
2023-11-15 13:26:46 +0900

    데이터베이스에 정보 저장
    회원 가입 조건 충족 시 FirebaseFireStore에 Name, Email, Password 추가
    User별로 각각 저장, 문서 이름은 각 UserID

--- ARconstruction/Assets/SJH/AuthManager.cs
+++ ARconstruction/Assets/SJH/AuthManager.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 using Firebase;
 using Firebase.Auth;
 using Firebase.Firestore;
+using Firebase.Extensions;
 using TMPro;
 using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using UnityEngine.SceneManagement;
 
 public class AuthManager : MonoBehaviour
 {
+    [Header("SignUp Panel")]
     [SerializeField] TMP_InputField userName;
     [SerializeField] TMP_InputField userEmail;
     [SerializeField] TMP_InputField userPassword;
     [SerializeField] TMP_InputField confirmPassword;
 
+    [Header("SignIn Panel")]
+    [SerializeField] TMP_InputField signInName;
+    [SerializeField] TMP_InputField signInPassword;
+
+
     FirebaseUser user;
     FirebaseAuth auth;
+    FirebaseFirestore db;
 
     string displayName;
     string emailAddress;
-
+    string userID;
 
     private void Start()
     {
@@ -27,7 +37,8 @@ public class AuthManager : MonoBehaviour
     }
     void InitializeFirebase()
     {
-        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        db = FirebaseFirestore.DefaultInstance;
+        auth = FirebaseAuth.DefaultInstance;
         auth.StateChanged += AuthStateChanged;
         AuthStateChanged(this, null);
     }
@@ -62,7 +73,10 @@ public class AuthManager : MonoBehaviour
     {
         if (userName != null)
         {
-            if (userPassword.text == confirmPassword.text) { UserSignUp(); }
+            if (userPassword.text == confirmPassword.text) 
+            {
+                UserSignUp();
+            }
             else { print("패스워드가 일치하지 않음"); }
         }
         else
@@ -87,10 +101,39 @@ public class AuthManager : MonoBehaviour
 
             // Firebase user has been created.
             AuthResult result = task.Result;
+            userID = result.User.UserId;
+            SendData(this);
             Debug.LogFormat("Firebase user created successfully: {0} ({1})",
                 result.User.DisplayName, result.User.UserId);
         });
     }
 
+    private static void SendData(AuthManager instance)
+    {
+        DocumentReference docRef = instance.db.Collection("users").Document(instance.userID);
+        Dictionary<string, object> user = new Dictionary<string, object>
+{
+        { "Name", instance.userName.text },
+        { "Password", instance.userPassword.text },
+        { "Email", instance.userEmail.text },
+};
+        docRef.SetAsync(user).ContinueWithOnMainThread(task => {
+            Debug.Log("Added data to the alovelace document in the users collection.");
+        });
+    }
+
+    public class UserData
+    {
+        public string userName;
+        public string eMail;
+        public string passWord;
+
+        public UserData(string userName, string eMail, string passWord)
+        {
+            this.userName = userName;
+            this.eMail = eMail;
+            this.passWord = passWord;
+        }
+    }
 
 }
